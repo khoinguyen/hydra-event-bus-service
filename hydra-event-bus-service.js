60fde21d4ccbe10f0aae8fe65366bdfe4ef28169
Khoi Nguyen <khoi.nguyen@primus.vn>
2018-08-08 14:39:58 +0700

    healthcheck for service

--- hydra-event-bus-service.js
+++ hydra-event-bus-service.js
@@ -11,15 +11,25 @@ hydraExpress.use(new HydraExpressLogger());
 
 const util = require('./lib/util');
 const ServerResponse = require('./lib/express-server-response');
+const registerMiddlewareCallback = () => {
+  const app = hydraExpress.getExpressApp();
+
+  app.use((req, res, next) => {
+    if (req.path == '/healthz') {
+      return res.status(200).send('OK');
+    }
+    next();
+  });
+}
 /**
  * Load configuration file
  */
 const configHelper = require('./lib/config-helper');
-hydraExpress.init(configHelper, () => {
+hydraExpress.init(configHelper, configHelper.serviceVersion, () => {
     hydraExpress.registerRoutes({
       '/v1/hydra-event-bus': require('./routes/hydra-event-v1-routes')
     });
-  })
+  }, registerMiddlewareCallback)
   .then(serviceInfo => {
     const logEntry = `Starting ${serviceInfo.serviceName} (v.${serviceInfo.serviceVersion})`;
 
